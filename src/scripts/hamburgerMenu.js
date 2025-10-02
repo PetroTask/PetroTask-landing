796c2c28a195104c33500a02491b2ca3b598535b
Trevor <valentinosolischang@gmail.com>
2025-10-02 18:16:10 -0500

    Enhance hamburger menu functionality by adding icon toggle for open and close states, improving user experience and accessibility.

--- src/scripts/hamburgerMenu.js
+++ src/scripts/hamburgerMenu.js
@@ -1,12 +1,23 @@
-// Menú hamburguesa
 document.addEventListener("DOMContentLoaded", () => {
   const navbarToggle = document.querySelector(".navbar__toggle");
   const navbarMenu = document.querySelector(".navbar__menu");
   const navbarLinks = document.querySelectorAll(".navbar__link");
+  const openMenuIcon = document.querySelector(".icon-open");
+  const closeMenuIcon = document.querySelector(".icon-close");
 
   // Toggle del menú hamburguesa
   navbarToggle.addEventListener("click", () => {
     navbarMenu.classList.toggle("active");
+
+    const isMenuActive = navbarMenu.classList.contains("active");
+
+    if (isMenuActive) {
+      openMenuIcon.style.display = "none";
+      closeMenuIcon.style.display = "block";
+    } else {
+      openMenuIcon.style.display = "block";
+      closeMenuIcon.style.display = "none";
+    }
 
     // Cambiar aria-expanded para accesibilidad
     const isExpanded = navbarMenu.classList.contains("active");
